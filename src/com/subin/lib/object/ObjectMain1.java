d787b1b5526692462e12be27af03f9b07586ed77
Ji-subin-97 <qls1178@naver.com>
2021-07-23 17:18:53 +0900

    Initial commit

---
+++ src/com/subin/lib/object/ObjectMain1.java
@@ -0,0 +1,26 @@
+package com.subin.lib.object;
+
+public class ObjectMain1 {
+
+	public static void main(String[] args) {
+		//객체 생성
+		//클래스명 참조변수명 = new 생성자();
+		
+		Object object = new Object();
+		
+		//멤버변수, 멤버메서드 호출
+		//참조변수명.변수명
+		//참조변수명.메서드명([인자값]);
+		
+		int num = object.hashCode();
+		System.out.println(num);
+		
+		String str = object.toString();
+		System.out.println(str);
+		System.out.println(object);
+		
+		boolean a = object.equals(object);
+		System.out.println(a);
+	}
+
+}
